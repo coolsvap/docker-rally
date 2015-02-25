ba2d77394857a8c425254141b1e302655aa5c53f
Swapnil Kulkarni <coolsvap@gmail.com>
2015-02-25 20:20:14 +0530

    Update init-rally.sh

--- init-rally.sh
+++ init-rally.sh
@@ -3,4 +3,4 @@
 /usr/local/bin/rally deployment create --filename=/home/rally/deployment.json --name=deployment
 /usr/local/bin/rally verify start
 /usr/local/bin/rally verify results --html --output-file=/home/rally/results.html
-/usr/local/bin/rally verify results --html --output-file=/home/rallyresults.json
+/usr/local/bin/rally verify results --json --output-file=/home/rally/results.json
